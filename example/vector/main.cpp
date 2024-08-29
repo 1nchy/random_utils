30f53e6f7018cd00cafc35351fd66cc7ed320396
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-08-29 17:01:19 +0800

    feat(example): add use case for wrapper::call

--- example/vector/main.cpp
+++ example/vector/main.cpp
@@ -6,10 +6,12 @@
 int main() {
     icy::container::wrapper<sum> _w;
     _w.enroll("sum3", &sum::sum3);
+    _w.enroll("sum2", &sum::sum2);
     _w.enroll("sum1", &sum::sum1);
     _w.enroll("sum0", &sum::sum0);
     _w.random_call("sum3");
     _w.random_call("sum1");
     _w.random_call("sum0");
+    auto _r2 = _w.call<int>("sum2", 2, 4);
     return 0;
 }
