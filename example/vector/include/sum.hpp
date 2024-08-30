c34f4a44ad7ad30339df2bf2a35f301182f0d17f
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-08-30 15:41:00 +0800

    feat(example): update example add function

--- example/vector/include/sum.hpp
+++ example/vector/include/sum.hpp
@@ -2,9 +2,9 @@
 #define _ICY_CONTAINER_TEST_EXAMPLE_SUM_HPP_
 
 struct sum {
-    int sum3(int a, int b, int c);
-    int sum2(int a, int b);
-    int sum1(int a);
+    int sum3(const int& a, const int& b, const int& c);
+    int sum2(const int a, const int b);
+    int sum1(int& a);
     int sum0();
     unsigned check() const;
 private:
--- example/vector/src/sum.cpp
+++ example/vector/src/sum.cpp
@@ -2,17 +2,17 @@
 
 #include <iostream>
 
-int sum::sum3(int a, int b, int c) {
+int sum::sum3(const int& a, const int& b, const int& c) {
     std::cout << "sum3(" << a << "+" << b << "+" << c << ")" << std::endl;
     _total += (a + b + c);
     return a + b + c;
 }
-int sum::sum2(int a, int b) {
+int sum::sum2(const int a, const int b) {
     std::cout << "sum2(" << a << "+" << b << ")" << std::endl;
     _total += (a + b);
     return a + b;
 }
-int sum::sum1(int a) {
+int sum::sum1(int& a) {
     std::cout << "sum1(" << a << ")" << std::endl;
     _total += (a);
     return a;
