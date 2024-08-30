e5fc8c8cc5e97295de93eacee8fe5ddb97f8d71a
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-08-30 21:44:15 +0800

    feat(example): add more functions to test

---
+++ example/vector/include/cat.hpp
@@ -0,0 +1,17 @@
+#ifndef _ICY_CONTAINER_TEST_EXAMPLE_CAT_HPP_
+#define _ICY_CONTAINER_TEST_EXAMPLE_CAT_HPP_
+
+#include <string>
+
+struct cat {
+    std::string cat3(const char& a, const char& b, const char& c);
+    std::string cat2(const char a, const char b);
+    std::string cat1(char& a);
+    std::string cat0();
+    char cat000(char& a) const;
+    unsigned check() const;
+private:
+    int _total = 0;
+};
+
+#endif // _ICY_CONTAINER_TEST_EXAMPLE_CAT_HPP_
--- example/vector/main.cpp
+++ example/vector/main.cpp
@@ -2,17 +2,30 @@
 #include "container_wrapper.hpp"
 
 #include "sum.hpp"
+#include "cat.hpp"
 
 int main() {
-    icy::container::wrapper<sum> _w;
-    _w.enroll("sum3", &sum::sum3);
-    _w.enroll("sum2", &sum::sum2);
-    _w.enroll("sum1", &sum::sum1);
-    _w.enroll("sum0", &sum::sum0, 3.0);
-    // _w.call_with_random_args("sum3");
-    // _w.call_with_random_args("sum1");
-    // _w.call_with_random_args("sum0");
-    // auto _r2 = _w.call<int>("sum2", 2, 4);
+    // icy::container::wrapper<sum> _w;
+    // _w.enroll("sum3", &sum::sum3);
+    // _w.enroll("sum2", &sum::sum2);
+    // _w.enroll("sum1", &sum::sum1);
+    // _w.enroll("sum0", &sum::sum0, 3.0);
+    // // _w.call_with_random_args("sum3");
+    // // _w.call_with_random_args("sum1");
+    // // _w.call_with_random_args("sum0");
+    // // auto _r2 = _w.call<int>("sum2", 2, 4);
+    // _w.run(30);
+
+    icy::container::wrapper<cat> _w;
+    _w.enroll("cat3", &cat::cat3);
+    _w.enroll("cat2", &cat::cat2);
+    _w.enroll("cat1", &cat::cat1);
+    _w.enroll("cat0", &cat::cat0, 3.0);
+    _w.enroll("cat000", &cat::cat000);
+    char _c;
+    _w.call<std::string>("cat1", _c);
+    _w.call<std::string>("cat0");
+    _w.call<char>("cat000", _c);
     _w.run(30);
     return 0;
 }
