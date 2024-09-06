ab37419251a6578fd8a1dfadbb8fc593ad9261ae
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-09-06 11:18:10 +0800

    feat(random object): custom static bound_type member

--- include/random_object.hpp
+++ include/random_object.hpp
@@ -27,7 +27,7 @@ inline size_t _S_diff_to_0(char _c) {
 void _S_init_random_seed();
 
 struct random_object_base;
-template <typename _Tp> struct random_object;
+template <typename _Tp, typename _Enable = void> struct random_object;
 
 
 
@@ -35,49 +35,103 @@ struct random_object_base {
     random_object_base();
     virtual ~random_object_base() = default;
 };
-template <typename _Tp> struct random_object : public random_object_base {
+
+/**
+ * @brief partial specialization for integral
+ * @tparam _Tp integral type
+*/
+template <typename _Tp> struct random_object<_Tp, typename std::enable_if<std::is_integral<_Tp>::value>::type> : public random_object_base {
     using obj_type = _Tp;
     using bound_type = _Tp;
+    static inline bound_type lb = 0; // lower bound
+    static inline bound_type ub = 2; // upper bound
     /**
      * @brief return random integral in [_l, _u)
      */
-    template <typename _U = obj_type> requires std::is_integral<_U>::value auto
-    rand(bound_type _l = 0, bound_type _u = 2) const -> obj_type {
-        static_assert(std::is_same<_U, obj_type>::value);
+    auto rand(bound_type _l, bound_type _u) const -> obj_type {
         assert(_l < _u);
         return obj_type(std::rand() % (_u - _l) + _l);
     };
     /**
+     * @brief return random integral
+     */
+    inline auto rand() const -> obj_type {
+        return rand(lb, ub);
+    };
+};
+template <typename _Tp> using random_integral_object = random_object<_Tp, typename std::enable_if<std::is_integral<_Tp>::value>::type>;
+
+/**
+ * @brief partial specialization for floating point
+ * @brief _Tp floating point type
+ */
+template <typename _Tp> struct random_object<_Tp, typename std::enable_if<std::is_floating_point<_Tp>::value>::type> : public random_object_base {
+    using obj_type = _Tp;
+    using bound_type = _Tp;
+    static inline bound_type lb = 0.0; // lower bound
+    static inline bound_type ub = 1.0; // upper bound
+    /**
      * @brief return random floating point in [_l, _u)
      */
-    template <typename _U = obj_type> requires std::is_floating_point<_U>::value auto
-    rand(bound_type _l = 0.0, bound_type _u = 1.0) const -> obj_type {
-        static_assert(std::is_same<_U, obj_type>::value);
+    auto rand(bound_type _l, bound_type _u) const -> obj_type {
         assert(_l < _u);
         return obj_type(static_cast<obj_type>(std::rand()) / RAND_MAX * (_u - _l) + _l);
     };
+    /**
+     * @brief return random floating point
+     */
+    inline auto rand() const -> obj_type {
+        return rand(lb, ub);
+    };
 };
+template <typename _Tp> using random_floating_point_object = random_object<_Tp, typename std::enable_if<std::is_floating_point<_Tp>::value>::type>;
+
+/**
+ * @brief partial specialization for char
+ */
 template <> struct random_object<char> : public random_object_base {
     using obj_type = char;
     using bound_type = char;
+    static inline bound_type lb = 'a'; // lower bound
+    static inline bound_type ub = 'z'; // upper bound
     /**
      * @brief return random char in [_l, _u]
+     * @param _l from 0 to 9, from A to Z, from a to z
+     * @param _u from 0 to 9, from A to Z, from a to z
      */
-    auto rand(bound_type _l = '0', bound_type _u = 'z') const -> obj_type {
+    auto rand(bound_type _l, bound_type _u) const -> obj_type {
         assert(_l < _u);
         size_t _low = _S_diff_to_0(_l);
         size_t _up = _S_diff_to_0(_u);
         assert(0 <= _low && _up < sizeof(_s_characters));
         return _s_characters[_low + std::rand() % (_up - _low + 1)];
     };
+    /**
+     * @brief return random char
+     */
+    inline auto rand() const -> obj_type {
+        return rand(lb, ub);
+    };
 };
+
+/**
+ * @brief partial specialization for std::string
+ */
 template <> struct random_object<std::string> : public random_object_base {
     using obj_type = std::string;
-    using bound_type = char;
+    using bound_type = std::string::value_type;
+    static inline bound_type lb = 'a'; // lower bound
+    static inline bound_type ub = 'z'; // upper bound
+    static inline size_t llb = 3; // lower length bound
+    static inline size_t ulb = 4; // upper length bound
     /**
      * @brief return random string, with elements in [_l, _u] and size in [_ll, _ul)
+     * @param _ll lower length bound
+     * @param _ul upper length bound
+     * @param _l from 0 to 9, from A to Z, from a to z
+     * @param _u from 0 to 9, from A to Z, from a to z
      */
-    auto rand(size_t _ll = 3, size_t _ul = 4, bound_type _l = 'a', bound_type _u = 'z') const -> obj_type {
+    auto rand(size_t _ll, size_t _ul, bound_type _l, bound_type _u) const -> obj_type {
         assert(_l < _u);
         size_t _length = _ulro.rand(_ll, _ul);
         std::string _str(_length, 0);
@@ -86,10 +140,25 @@ template <> struct random_object<std::string> : public random_object_base {
         }
         return _str;
     };
+    /**
+     * @brief return random string, with size in [_ll, _ul)
+     * @param _ll lower length bound
+     * @param _ul upper length bound
+     */
+    inline auto rand(size_t _ll, size_t _ul) const -> obj_type {
+        return rand(_ll, _ul, lb, ub);
+    };
+    /**
+     * @brief return random string
+     */
+    inline auto rand() const -> obj_type {
+        return rand(llb, ulb, lb, ub);
+    };
 private:
     random_object<size_t> _ulro;
     random_object<char> _cro;
 };
+
 template <> struct random_object<void> : public random_object_base {
     using object_type = size_t;
     using bound_type = double;
