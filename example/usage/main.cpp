28a880df8fce44d1089c1ec3cdffa5343d13ab62
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-09-06 11:18:38 +0800

    feat(example): update usage in example

--- example/usage/main.cpp
+++ example/usage/main.cpp
@@ -5,30 +5,34 @@
 #include "cat.hpp"
 
 int main() {
-    // icy::container::wrapper<sum> _w;
-    // _w.enroll("sum3", &sum::sum3);
-    // _w.enroll("sum2", &sum::sum2);
-    // _w.enroll("sum1", &sum::sum1);
-    // _w.enroll("sum0", &sum::sum0, 3.0);
-    // // _w.call_with_random_args("sum3");
-    // // _w.call_with_random_args("sum1");
-    // // _w.call_with_random_args("sum0");
-    // // auto _r2 = _w.call<int>("sum2", 2, 4);
-    // _w.run(30);
+    icy::random_object<int>::lb = -1;
+    icy::random_integral_object<int>::ub = 3;
+    icy::container::wrapper<sum> _ws;
+    _ws.enroll("sum3", &sum::sum3);
+    _ws.enroll("sum2", &sum::sum2);
+    _ws.enroll("sum1", &sum::sum1);
+    _ws.enroll("sum0", &sum::sum0, 3.0);
+    // _ws.call_with_random_args("sum3");
+    // _ws.call_with_random_args("sum1");
+    // _ws.call_with_random_args("sum0");
+    // auto _r2 = _ws.call<int>("sum2", 2, 4);
+    _ws.run(20);
 
-    icy::container::wrapper<cat> _w;
-    _w.enroll("cat3", &cat::cat3);
-    _w.enroll("cat2", &cat::cat2);
-    _w.enroll("cat1", &cat::cat1);
-    _w.enroll("cat0", &cat::cat0, 3.0);
-    _w.enroll("cat000", &cat::cat000);
+    icy::container::wrapper<cat> _wc;
+    icy::random_object<char>::lb = 'a';
+    icy::random_object<char>::ub = 'f';
+    _wc.enroll("cat3", &cat::cat3);
+    _wc.enroll("cat2", &cat::cat2);
+    _wc.enroll("cat1", &cat::cat1);
+    _wc.enroll("cat0", &cat::cat0, 3.0);
+    _wc.enroll("cat000", &cat::cat000);
     char _c;
-    // _w.call<std::string>("cat1", _c);
-    // _w.call<std::string>("cat0");
-    // _w.call<char>("cat000", _c);
-    // _w.log_file("main.txt");
-    _w.enable_log();
-    _w.run(30);
+    // _wc.call<std::string>("cat1", _c);
+    // _wc.call<std::string>("cat0");
+    // _wc.call<char>("cat000", _c);
+    _wc.log_file("main.txt");
+    _wc.enable_log();
+    _wc.run(30);
     return 0;
 
 
