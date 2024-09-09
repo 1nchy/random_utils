a348eefea47a950d786e0462c915c6da1d3abcc8
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-09-09 14:44:35 +0800

    feat(example): add usage for random pair object

--- example/usage/include/sum.hpp
+++ example/usage/include/sum.hpp
@@ -1,11 +1,14 @@
 #ifndef _ICY_CONTAINER_WRAPPER_EXAMPLE_SUM_HPP_
 #define _ICY_CONTAINER_WRAPPER_EXAMPLE_SUM_HPP_
 
+#include <utility>
+
 struct sum {
     int sum3(const int& a, const int& b, const int& c);
     int sum2(const int a, const int b);
     int sum1(int& a);
-    int sum0();
+    int sum_pair(const std::pair<unsigned, float>& _p);
+    int sum0() const;
     unsigned check() const;
 private:
     int _total = 0;
