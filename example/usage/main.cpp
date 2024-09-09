b38d30fcbb81c6b601a7f2e720683005498fac8f
1nchy <wangyinqi23@mails.ucas.ac.cn>
2024-09-09 19:15:44 +0800

    feat(example): add usage for random tuple object

--- example/usage/main.cpp
+++ example/usage/main.cpp
@@ -4,35 +4,43 @@
 #include "sum.hpp"
 #include "cat.hpp"
 
-template <typename _T1, typename _T2> std::ostream& operator<<(std::ostream& _os, const std::pair<_T1, _T2>& _p) {
-    _os << '(' << _p.first << ", " << _p.second << ')';
-    return _os;
-}
-
 int main() {
-    icy::random_object<std::pair<const unsigned, std::string>> _ro;
+    icy::random_object<std::pair<const unsigned, std::string>> _ro1;
     for (size_t _i = 0; _i < 10; ++_i) {
-        const std::pair<unsigned, std::string>& _p = _ro.rand(
+        // const std::pair<unsigned, std::string>& _p = _ro1.rand(
+        const auto& _p = _ro1.rand(
+            // std::forward_as_tuple(0, 4),
+            // std::forward_as_tuple(2, 5, 'A', 'F')
+        );
+        icy::container::operator<<(std::cout, _p);
+        std::cout << std::endl;
+    }
+    icy::random_object<std::tuple<unsigned, char, std::string>> _ro2;
+    for (size_t _i = 0; _i < 10; ++_i) {
+        // const std::tuple<unsigned, char, std::string>& _p = _ro2.rand(
+        const auto& _p = _ro2.rand(
             std::forward_as_tuple(0, 4),
+            std::forward_as_tuple(),
             std::forward_as_tuple(2, 5, 'A', 'F')
         );
-        std::cout << _p << std::endl;
+        icy::container::operator<<(std::cout, _p);
+        std::cout << std::endl;
     }
-    icy::random_object<int>::lb = -1;
-    icy::random_integral_object<int>::ub = 3;
-    icy::container::wrapper<sum> _ws;
-    _ws.enroll("sum3", &sum::sum3);
-    _ws.enroll("sum2", &sum::sum2);
-    _ws.enroll("sum1", &sum::sum1);
-    _ws.enroll("sum_pair", &sum::sum_pair);
-    _ws.enroll("sum0", &sum::sum0, 3.0);
-    // _ws.call_with_random_args("sum3");
-    // _ws.call_with_random_args("sum1");
-    // _ws.call_with_random_args("sum0");
-    // auto _r2 = _ws.call<int>("sum2", 2, 4);
-    _ws.log_file("sum.txt");
-    _ws.enable_log();
-    _ws.run(20);
+    // icy::random_object<int>::lb = -1;
+    // icy::random_integral_object<int>::ub = 3;
+    // icy::container::wrapper<sum> _ws;
+    // _ws.enroll("sum3", &sum::sum3);
+    // _ws.enroll("sum2", &sum::sum2);
+    // _ws.enroll("sum1", &sum::sum1);
+    // _ws.enroll("sum_pair", &sum::sum_pair);
+    // _ws.enroll("sum0", &sum::sum0, 3.0);
+    // // _ws.call_with_random_args("sum3");
+    // // _ws.call_with_random_args("sum1");
+    // // _ws.call_with_random_args("sum0");
+    // // auto _r2 = _ws.call<int>("sum2", 2, 4);
+    // _ws.log_file("sum.txt");
+    // _ws.enable_log();
+    // _ws.run(20);
 
     // icy::container::wrapper<cat> _wc;
     // icy::random_object<char>::lb = 'a';
